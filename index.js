3a89d91940b85a9ba50bfcdd925e19550043fcbc
Maurice Nganga <61679467+moryno@users.noreply.github.com>
2022-12-10 13:03:40 +0300

    create routes for auth, users, posts, likes and comments to communicate with the client browser

--- index.js
+++ index.js
@@ -2,11 +2,23 @@ import express from "express";
 import cors from "cors";
 import cookieParser from "cookieparser";
 
+import authRoutes from "./routes/auth.js";
+import userRoutes from "./routes/users.js";
+import likeRoutes from "./routes/likes.js";
+import commentRoutes from "./routes/comments.js";
+import postRoutes from "./routes/posts.js";
+
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 // app.use(cookieParser());
+
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/posts", postRoutes);
+app.use("/api/likes", likeRoutes);
+app.use("/api/comments", commentRoutes);
 
 app.listen(3030, () => {
   console.log("Backend running at port 3030");
